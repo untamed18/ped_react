20584ddb2a23d83e304a013ac19c0a241578b531
untamed18 <seunopeyemi16@gmail.com>
2025-06-12 23:49:40 +0100

    blog app update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -50,13 +50,24 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <h3 className="text-xl font-bold mb-2">Full stack Blog App</h3>
               <p className="text-gray-400 mb-4">
-                ML-powered data visualization platform with predictive analytics
-                and interactive reports.
+                A web application that allows users to read blogs and share
+                comments. It features an admin dashboard where administrators
+                can create, publish, or unpublish blog posts, as well as view,
+                approve, or disapprove reader comments. The platform also
+                includes AI integration that assists users in generating blog
+                content using artificial intelligence.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                {[
+                  "React",
+                  "Node.js",
+                  "Express.js",
+                  "MongoDb",
+                  "Vercel",
+                  "Gemini AI",
+                ].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -95,22 +106,20 @@ export const Projects = () => {
                 integration, and customizable product inventory.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
-                  (tech) => (
-                    <span
-                      key={tech}
-                      className="
+                {["Next.js", "React", "Stripe", "MongoDb"].map((tech) => (
+                  <span
+                    key={tech}
+                    className="
                       bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
                       hover:bg-blue-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
                     "
-                    >
-                      {tech}
-                    </span>
-                  )
-                )}
+                  >
+                    {tech}
+                  </span>
+                ))}
               </div>
               <div className="flex justify-between items-center">
                 <a
